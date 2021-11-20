a8d181bddd255e31d143c296840c4503be6ea0e7
gerritezra <67199284+gerritezra@users.noreply.github.com>
2021-11-20 11:50:03 +0700

    Update myNavbar.js

--- src/components/myNavbar.js
+++ src/components/myNavbar.js
@@ -5,7 +5,7 @@ const myNavbar = () => {
         <div>
       <Navbar bg="dark" expaind="lg" variant="dark">
         <Container>
-          <Navbar.Brand href="#">Mupilis</Navbar.Brand>
+          <Navbar.Brand href="#">Temukan Film Kuy</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
